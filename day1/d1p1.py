78e70281c4d5df1f4dc7c0ee9a208b0df9ea513f
core-dump-19700101 <noein0122@gmail.com>
2021-12-01 09:55:14 -0800

    Adding day 1 input and solution

---
+++ day1/d1p1.py
@@ -0,0 +1,12 @@
+count = 0
+prev_depth = -1
+with open('Day1/input') as f:
+    line = f.readline()
+    while line:
+        depth = int(line)
+        if (prev_depth > -1 and depth > prev_depth):
+            count += 1
+        prev_depth = depth
+        line = f.readline()
+
+print("Increased " + str(count) + " times")
---
+++ day1/d1p2.py
@@ -0,0 +1,18 @@
+WINDOW_SIZE = 3
+
+with open('Day1/input') as f:
+    lines = f.readlines()
+    i = 0
+    prev_sum = -1
+    count = 0
+    while i <= len(lines) - WINDOW_SIZE:
+        sum = 0
+        #print("slice=" + str(lines[i:i+WINDOW_SIZE]))
+        for line in lines[i:i+WINDOW_SIZE]:
+            sum += int(line)
+        #print("prev_sum=%d, sum=%d" %(prev_sum, sum))
+        if prev_sum > -1 and sum > prev_sum:
+            count += 1
+        prev_sum = sum
+        i += 1
+    print("Increased " + str(count) + " times")
