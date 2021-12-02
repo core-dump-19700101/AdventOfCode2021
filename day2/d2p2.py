88b1e368d33d682a6ee19f2ab6151227c839f189
core-dump-19700101 <noein0122@gmail.com>
2021-12-02 15:13:46 -0800

    Add day2 input and solution

---
+++ day2/d2p2.py
@@ -0,0 +1,25 @@
+def parse_file(file):
+    position = [0, 0, 0]
+    with open(file) as f:
+        for line in f.readlines():
+            position = calculate_position(parse_line(line), position)
+    return position[0]*position[1]
+
+def parse_line(line):
+    tokens = line.split()
+    return [tokens[0], int(tokens[1])]
+
+def calculate_position(command, current_position):
+    x = current_position[0]
+    y = current_position[1]
+    aim = current_position[2]
+    if (command[0] == 'up'):
+        aim -= command[1]
+    elif (command[0] == 'down'):
+        aim += command[1]
+    elif (command[0] == 'forward'):
+        x += command[1]
+        y += command[1] * aim
+    return [x, y, aim]
+
+print(parse_file('day2/input'))
