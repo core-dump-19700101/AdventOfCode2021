c494b07c2d4817d421067583cc1eeb60c0a78116
core-dump-19700101 <noein0122@gmail.com>
2021-12-07 12:11:31 -0800

    Limit target positions based on https://www.reddit.com/gallery/rawxad

--- src/solution/d7.py
+++ src/solution/d7.py
@@ -1,3 +1,5 @@
+import math
+
 def solve(input):
     positions = [int(n) for n in input[0].split(',')]
     return find_min_position(positions), find_min_position(positions, False)
@@ -9,12 +11,16 @@ def calc_fuel_cost(p, positions, linear=True):
         cost += base_cost if linear else base_cost * (base_cost+1) // 2
     return cost
 
+def get_target_positions(positions, linear=True):
+    if linear:
+        return positions
+    mean = sum(positions)/len(positions)
+    return set([math.floor(mean), math.ceil(mean)])
+
 def find_min_position(positions, linear=True):
-    positions.sort()
-    min, max = positions[0], positions[-1]
     cache = {}
     min_p = None
-    for p in range(min, max+1):
+    for p in get_target_positions(positions, linear):
         if p not in cache:
             cache[p] = calc_fuel_cost(p, positions, linear)
         if min_p == None:
