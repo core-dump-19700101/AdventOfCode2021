c3294f10f828d10cbba6db255cb63aa7c96b34a5
core-dump-19700101 <noein0122@gmail.com>
2021-12-07 12:01:17 -0800

    Add day 7 input and solution

---
+++ src/solution/d7.py
@@ -0,0 +1,24 @@
+def solve(input):
+    positions = [int(n) for n in input[0].split(',')]
+    return find_min_position(positions), find_min_position(positions, False)
+
+def calc_fuel_cost(p, positions, linear=True):
+    cost = 0
+    for i in range(0, len(positions)):
+        base_cost = abs(positions[i]-p)
+        cost += base_cost if linear else base_cost * (base_cost+1) // 2
+    return cost
+
+def find_min_position(positions, linear=True):
+    positions.sort()
+    min, max = positions[0], positions[-1]
+    cache = {}
+    min_p = None
+    for p in range(min, max+1):
+        if p not in cache:
+            cache[p] = calc_fuel_cost(p, positions, linear)
+        if min_p == None:
+            min_p = (p, cache[p])
+        elif min_p[1] > cache[p]:
+            min_p = (p, cache[p])
+    return min_p[1]
